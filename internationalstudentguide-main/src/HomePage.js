c8093e4e1c1e470c9ffbc28d3713e4232b62d0c6
Parteek Saini <sainiparteek18@gmail.com>
2021-10-21 21:21:03 -0700

    Re-added the visa info page data

--- internationalstudentguide-main/src/HomePage.js
+++ internationalstudentguide-main/src/HomePage.js
@@ -100,11 +100,19 @@ class HomePage extends Component {
             <br />
             <h5>Daily Updates!</h5>
             <p> </p>
-            <p>Do you know, International students’confidence in Canada growing due to successful COVID-19 vaccine program. <a href="https://www.immigration.ca/international-students-confidence-in-canada-growing-due-to-successful-covid-19-vaccine-program">Know more..</a></p>
-            <p>Do you know, International students defer to January 2022 due to late process time to issue study permit, therefore, they are forced to study online. <a href="https://thepienews.com/news/canada-international-students-defer-to-january-2022/">Know more..</a></p>
-            <p>Do you know, Visa extension in Canada for international students. <a href="https://www.studyinternational.com/news/visa-extension-in-canada/">Know more..</a></p>
-            <p>Do you know, New Brunswick launches new immigration pilot for international graduates. <a href="https://www.cicnews.com/2021/09/new-brunswick-launches-new-immigration-pilot-for-international-graduates-0919058.html#gs.cqcp7m">Know more..</a></p>
-            <p>Do you know, Spike in international student enrolment shows MUN's global appeal despite rise in tuition fees. <a href="https://www.cbc.ca/news/canada/newfoundland-labrador/international-enrolment-mun-2021-1.6176812">Know more..</a></p>
+            <p>Do you know, International students’confidence in Canada growing due to successful
+               COVID-19 vaccine program. 
+               <a href="https://www.immigration.ca/international-students-confidence-in-canada-growing-due-to-successful-covid-19-vaccine-program">Know more..</a></p>
+            <p>Do you know, International students defer to January 2022 due to late process time to issue
+               study permit, therefore, they are forced to study online. 
+               <a href="https://thepienews.com/news/canada-international-students-defer-to-january-2022/">Know more..</a></p>
+            <p>Do you know, Visa extension in Canada for international students. 
+              <a href="https://www.studyinternational.com/news/visa-extension-in-canada/">Know more..</a></p>
+            <p>Do you know, New Brunswick launches new immigration pilot for international graduates. 
+              <a href="https://www.cicnews.com/2021/09/new-brunswick-launches-new-immigration-pilot-for-international-graduates-0919058.html#gs.cqcp7m">Know more..</a></p>
+            <p>Do you know, Spike in international student enrolment shows MUN's global appeal despite
+               rise in tuition fees. 
+               <a href="https://www.cbc.ca/news/canada/newfoundland-labrador/international-enrolment-mun-2021-1.6176812">Know more..</a></p>
             <p>Do you know, Canada universities and colleges to accept Indian board results. <a href="https://timesofindia.indiatimes.com/city/lucknow/canada-universities-colleges-to-accept-indian-board-results/articleshow/86665877.cms">Know more..</a></p>
             {/* <p>Do you know, <a href="">Know more..</a></p> */}
           </div>
--- internationalstudentguide-main/src/visainfo.js
+++ internationalstudentguide-main/src/visainfo.js
@@ -1,13 +1,61 @@
 import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import CollapsiblePanel from './components/CollapsiblePanel';
+import 'bootstrap/dist/css/bootstrap.css';
+import VisainfoData from './VisainfoData.json';
 
-
-function visaInfo() {
+//adding to git again
+function VisaInfo() {
+  const [collapse, setCollapse] = useState(true);
+  const [title, setTitle] = useState("Expand All");
+  const [icon, setIcon] = useState("fa fa-chevron-right");
+  const collapseAll = () => {
+      setCollapse(!collapse);
+      setIcon(state => {
+          return state === "fa fa-chevron-right"
+              ? "fa fa-chevron-down"
+              : "fa fa-chevron-right";
+      });
+      setTitle(state => {
+          return state === "Expand All" ? "Collapse All" : "Expand All";
+      });
+  };
   return (
-     <div>
-       <img alt="Example Alt" src="/assests/process.png" />
+     <div lassName="App container my-2">
+       <img alt="process" src="/assests/process.png" />
+       <button
+              type="button"
+              className="btn-warning mt-4 mb-3 float-left"
+              onClick={collapseAll}
+          >
+              <i className={icon} /> {title}
+          </button>
+          
+          {VisainfoData.map((doc, index) => (
+            <div key={index}>
+                <CollapsiblePanel title={doc.title} collapse={collapse}>
+                  {doc.documentFirst.map((d1, i) => (
+                    <div key={i}>
+                      <CollapsiblePanel title={d1.documentName} collapse={collapse}>
+                        {d1.documentDetails.map((de, id) => (
+                          <div key={id}>
+                            {de.details1}
+                            <br />
+                            {de.details2}
+                            <br />
+                            {de.details3}
+                            <br/>
+                            {de.details4}
+                          </div>
+                        ) )}
+                      </CollapsiblePanel>
+                    </div>
+                  ))}
+                </CollapsiblePanel>
+            </div>
+          ))}
      </div>
   );
 }
-
-
-export default visaInfo;
+export default VisaInfo;
